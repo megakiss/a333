c2bb594c4c336a34e143a722973e37bd07bf6d40
megakiss <bbuggu99@gmail.com>
2021-09-01 17:48:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type' : 'text/plain' });
-  res.end('Hello World!!+m');
+  res.end('Hello megakiss');
 });
 
 server.listen(8000);
